d4801ad6d47b47cbe8c101b9267d678cedef0676
Matheus <zTeussandi@hotmail.com>
2021-12-20 15:15:05 -0300

    Update styles.ts

--- src/components/Dashboard/styles.ts
+++ src/components/Dashboard/styles.ts
@@ -6,7 +6,6 @@ export const Container = styled.div`
     gap: 2rem;
     margin-top: -7em;
     
-
     div {
         background-color: #fff;
         padding: 1.5rem 2rem;
@@ -14,7 +13,6 @@ export const Container = styled.div`
         color: #000;
         margin: 2rem;
     
-
         header {
             font-family: 'Roboto', sans-serif;
             display: flex;
@@ -38,36 +36,4 @@ export const Container = styled.div`
             color: #000;
         }
     }
-
-    .bt1 {
-        color: #ffffffff;
-        background-color: #ff0020;
-    }
-    .bt2 {
-        color: #ffffffff;
-        background-color: #0012ff;
-    }
-
-    .bt3 {
-        color: #ffffffff;
-        background-color: #ff0020;
-    }
-
-    button {
-        max-width: 1120px;
-        
-        font-size: 1rem;
-        border: 0;
-        padding: 0 2rem;
-        border-radius: 0.25rem;
-        height: 2.5rem;
-        margin: 2rem;
-
-        transition: filter 0.3s;
-
-        &:hover {
-            filter: brightness(0.7);
-        }
-    }
-
 `
