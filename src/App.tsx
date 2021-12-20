f9c7838812783e43b0889060003853b9ae0d058a
Matheus <zTeussandi@hotmail.com>
2021-12-20 15:15:46 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import { GlobalStyle } from "./styles/global";
+import { Share } from "./components/Share";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <GlobalStyle />
-      <Dashboard />
+        <GlobalStyle />
+          <Dashboard />
+            <Share />
     </div>
   );
 }
